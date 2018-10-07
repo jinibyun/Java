fae6034e1ce00fca7875546123e9e50ebbffa34c
Jini Byun <jinibyun@gmail.com>
2018-10-07 15:24:41 -0400

    commit

---
+++ Tutorial/src/javaLecture/Ch01_Ex01.java
@@ -0,0 +1,13 @@
+/*
+Basic Starting Point 
+*/
+package javaLecture;
+
+public class Ch01_Ex01{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word"); 
+	}
+
+}
---
+++ Tutorial/src/javaLecture/Ch01_Ex02.java
@@ -0,0 +1,22 @@
+/*
+Variable and  data type
+*/
+package javaLecture;
+
+public class Ch01_Ex02 {
+
+	public static void main(String[] args) {		
+		int var1; // this declares a variable  
+	    int var2; // this declares another variable  
+	  
+	    var1 = 1024; // this assigns 1024 to var1  
+	  
+	    System.out.println("var1 contains " + var1);   
+	  
+	    var2 = var1 / 2;  
+	  
+	    System.out.print("var2 contains var1 / 2: ");  
+	    System.out.println(var2); 
+	}
+
+}
